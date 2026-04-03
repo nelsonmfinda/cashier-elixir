1dcdbd5889164cddafc8ddc8bafbc0e58045672f
Nelson Mfinda <cazukyjunior@gmail.com>
2026-04-03 22:31:04 +0100

    chore: initialise mix project with decimal, credo, dialyxir, excoveralls

---
+++ mix.exs
@@ -0,0 +1,55 @@
+defmodule Cashier.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :cashier,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      elixirc_options: [warnings_as_errors: true],
+      aliases: aliases(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger, :crypto],
+      mod: {Cashier.Application, []}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp aliases do
+    [
+      setup: ["deps.get", "git_hooks.install"]
+    ]
+  end
+
+  defp deps do
+    [
+      {:decimal, "~> 2.0"},
+      {:git_hooks, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:stream_data, "~> 1.0", only: :test}
+    ]
+  end
+end
